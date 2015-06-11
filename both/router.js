11bc9c267747fa83a819064ad6b4ea2f41d0c39f
unknown <jelalann@LFR007444.corp.capgemini.com>
2015-06-11 11:04:08 +0200

    first commit

---
+++ both/router.js
@@ -0,0 +1,16 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Meteor.startup(function () {
+  if (Meteor.isClient) {
+    var location = Iron.Location.get();
+    if (location.queryObject.platformOverride) {
+      Session.set('platformOverride', location.queryObject.platformOverride);
+    }
+  }
+});
+
+Router.map(function() {
+  this.route('index', {path: '/'});
+});
